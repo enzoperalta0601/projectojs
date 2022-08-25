dcbfeac075f7205a07dc49c6588f04a5ea91672e
Peralta <peraltaenzog@gmail.com>
2022-08-25 15:56:02 -0300

    se agrega projecto

---
+++ app.js
@@ -0,0 +1,33 @@
+//VARIABLES
+/*
+let nombre= "Micaela"; //string(cadena de texto)
+console.log(nombre);
+let edad= 28; //numerado
+let estudiante= true; //buleano(verdadero o falso)
+
+//CONSTANTES
+//deben ser declaradas con valor
+//no pueden ser re-declaras
+//deben ser declarada con mayuscula
+
+
+
+const APELLIDO= "Echeverria";
+
+
+
+//CONCATENACION
+let nombre="Micaela";
+let apellido="Echeverria";
+
+
+*/
+
+
+//prompt
+let nombre= prompt("ingrese su nombre");
+let edad= Number(prompt("años"))
+
+alert("HOLA " + nombre  + " tienes " + edad + " giros al sol");
+
+
