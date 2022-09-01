a9d6376ae6fd33ae65ec10bc47f7d42a2202706b
enzoperalta0601 <108321567+enzoperalta0601@users.noreply.github.com>
2022-09-01 10:39:57 -0300

    Add files via upload

---
+++ conversor.js
@@ -0,0 +1,27 @@
+var cotizacionDolar = 300;
+var cotizacionEuro = 315;
+
+//
+document.querySelector("#aplicarPesos").addEventListener("click", conversor);
+
+function conversor(e) {
+ e.preventDefault();
+ let pesos = "";
+ let moneda = "";
+
+ // Obtener divisa elegida por usuario
+ if (document.querySelector("#dolar").checked == true) {
+  montodivisa = cotizacionDolar;
+  moneda = "Dólares";
+ } else if (document.querySelector("#euro").checked == true) {
+  montodivisa = cotizacionEuro;
+  moneda = "Euros";
+ }
+
+ // Obtener monto en pesos que ingresó el usuario
+ pesos = document.querySelector("#pesos").value;
+
+ // Aplicar el resultado
+ document.querySelector("#resultado").value = pesos * montodivisa;
+ document.querySelector(".moneda").innerHTML = moneda;
+}
