d341c7c786f6f6282378ca77668e504f9231ee46
enzoperalta0601 <108321567+enzoperalta0601@users.noreply.github.com>
2022-09-06 18:43:53 -0300

    Add files via upload

--- conversor.js
+++ conversor.js
@@ -1,7 +1,33 @@
-var cotizacionDolar = 300;
-var cotizacionEuro = 315;
+var divisas = [
+ {
+  nombre: "Dólares",
+  id: "dolar",
+  cotizacion: 300,
+ },
+ {
+  nombre: "Euros",
+  id: "euro",
+  cotizacion: 315,
+ },
+ {
+  nombre: "Libras",
+  id: "libra",
+  cotizacion: 345,
+ },
+];
 
-//
+let formulario = document.querySelector(".opciones");
+
+for (const divisa of divisas) {
+ formulario.innerHTML =
+  formulario.innerHTML +
+  `<div class="form_group">
+        <input type="radio" name="moneda" id="${divisa.id}" value="${divisa.id}">
+        <label for="${divisa.id}">${divisa.nombre}</label>
+    </div>`;
+}
+
+// Boton para ejecutar funcion
 document.querySelector("#aplicarPesos").addEventListener("click", conversor);
 
 function conversor(e) {
@@ -10,12 +36,11 @@ function conversor(e) {
  let moneda = "";
 
  // Obtener divisa elegida por usuario
- if (document.querySelector("#dolar").checked == true) {
-  montodivisa = cotizacionDolar;
-  moneda = "Dólares";
- } else if (document.querySelector("#euro").checked == true) {
-  montodivisa = cotizacionEuro;
-  moneda = "Euros";
+ for (const divisa of divisas) {
+  if (document.querySelector("#" + divisa.id).checked == true) {
+   montodivisa = divisa.cotizacion;
+   moneda = divisa.nombre;
+  }
  }
 
  // Obtener monto en pesos que ingresó el usuario
