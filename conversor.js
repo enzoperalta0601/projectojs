c68f3f8422c483b7c1ed2b7197136cb09cfb07ac
enzoperalta0601 <108321567+enzoperalta0601@users.noreply.github.com>
2022-09-28 00:18:01 -0300

    Add files via upload

--- conversor.js
+++ conversor.js
@@ -1,52 +1,98 @@
+// Código nuevo
 var divisas = [
  {
+  id: 0,
   nombre: "Dólares",
-  id: "dolar",
+  simbolo: "USD",
   cotizacion: 300,
  },
  {
+  id: 1,
   nombre: "Euros",
-  id: "euro",
+  simbolo: "EUR",
   cotizacion: 315,
  },
  {
+  id: 2,
   nombre: "Libras",
-  id: "libra",
+  simbolo: "GBP",
   cotizacion: 345,
  },
 ];
 
-let formulario = document.querySelector(".opciones");
+// Contruir select
+const selectdivisa = document.querySelector("#divisa");
+const inputpesos = document.querySelector("#pesos");
+const inputotramoneda = document.querySelector("#otramoneda");
+const textomoneda = document.querySelector(
+ ".otramoneda_container .textomoneda"
+);
+const nombremoneda = document.querySelector(".nombremoneda");
 
-for (const divisa of divisas) {
- formulario.innerHTML =
-  formulario.innerHTML +
-  `<div class="form_group">
-        <input type="radio" name="moneda" id="${divisa.id}" value="${divisa.id}">
-        <label for="${divisa.id}">${divisa.nombre}</label>
-    </div>`;
+for (let divisa of divisas) {
+ selectdivisa.innerHTML =
+  selectdivisa.innerHTML +
+  `<option value="${divisa.id}">${divisa.nombre}</option>`;
 }
 
-// Boton para ejecutar funcion
-document.querySelector("#aplicarPesos").addEventListener("click", conversor);
+inputpesos.value = 0;
+inputotramoneda.value = 0;
 
-function conversor(e) {
- e.preventDefault();
- let pesos = "";
- let moneda = "";
+// Conversor
+function conversor(event) {
+ let current_divisa = selectdivisa.options[selectdivisa.selectedIndex].value;
+ let current_pesos = parseFloat(inputpesos.value) || 0;
+ let current_otramoneda = parseFloat(inputotramoneda.value) || 0;
 
- // Obtener divisa elegida por usuario
- for (const divisa of divisas) {
-  if (document.querySelector("#" + divisa.id).checked == true) {
-   montodivisa = divisa.cotizacion;
-   moneda = divisa.nombre;
+ if (current_divisa !== "") {
+  let current_cotizacion = divisas[current_divisa].cotizacion;
+  let current_simbolo = divisas[current_divisa].simbolo;
+  let current_nombre = divisas[current_divisa].nombre;
+
+  nombremoneda.innerHTML = current_nombre;
+
+  console.log(current_pesos);
+  if (
+   inputpesos.checkValidity() == false ||
+   inputotramoneda.checkValidity() == false
+  ) {
+   Toastify({
+    text: "Ingresá sólo números",
+    duration: 3000,
+    close: true,
+    gravity: "top",
+    position: "center",
+    stopOnFocus: true,
+    style: {
+     background: "linear-gradient(to right, #ED213A, #93291E)",
+    },
+   }).showToast();
+  } else {
+   if (event.target.id == "pesos") {
+    inputotramoneda.value = current_pesos / current_cotizacion;
+   } else if (event.target.id == "otramoneda") {
+    inputpesos.value = current_otramoneda * current_cotizacion;
+   } else if (event.target.id == "divisa" && current_pesos !== "") {
+    inputotramoneda.value = current_pesos / current_cotizacion;
+    textomoneda.innerHTML = current_simbolo;
+   }
   }
+ } else {
+  Toastify({
+   text: "Ingresá primero una divisa para hacer la conversión",
+   duration: 3000,
+   close: true,
+   gravity: "top",
+   position: "center",
+   stopOnFocus: true,
+   style: {
+    background: "linear-gradient(to right, #ED213A, #93291E)",
+   },
+  }).showToast();
  }
-
- // Obtener monto en pesos que ingresó el usuario
- pesos = document.querySelector("#pesos").value;
-
- // Aplicar el resultado
- document.querySelector("#resultado").value = pesos / montodivisa;
- document.querySelector(".moneda").innerHTML = moneda;
 }
+
+// Add event listener
+selectdivisa.addEventListener("change", conversor);
+inputpesos.addEventListener("keyup", conversor);
+inputotramoneda.addEventListener("keyup", conversor);
